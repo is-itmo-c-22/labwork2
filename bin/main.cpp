a0af963182d088959bff03d1b4381686287dc440
Vladimir Saltinsky <itisdecoy@gmail.com>
2022-10-09 23:48:09 +0300

    all were done except umnosh.

--- bin/main.cpp
+++ bin/main.cpp
@@ -3,9 +3,12 @@
 #include <iostream>
 
 int main() {
-    uint2022_t a = from_string("12345678912345678912456789123654789");
-    uint2022_t b = from_string("1000000");
-    std::cout << a + b;
+    uint2022_t a = from_string("405272312330606683982498447530407677486444946329741970511324085183808429621437");
+    uint2022_t b = from_string("1");
+//    std::cout << a << "    a---b   " << b << std::endl;
+//    std::cout << a << std::endl;
+    std::cout << a * b << std::endl;
+    std::cout << "1469832487054184013178321496623041557517329857560238757278117847507488415462666081345922349701550571520" << std::endl;
 
     return 0;
 }
--- lib/number.cpp
+++ lib/number.cpp
@@ -4,9 +4,12 @@
 
 uint2022_t from_uint(uint32_t i) {
     uint2022_t digits;
-    digits.number[0] = i;
+
+    digits.number[0] = i % 1000000000;
+    digits.number[1] = i / 1000000000;
     return digits;
 }
+
 
 
 uint2022_t from_string(const char* buff) {
@@ -23,27 +26,33 @@ uint2022_t from_string(const char* buff) {
         }
     }
 
-    int step = length;
-    int j = 0;
-
-    if (lengthMas > 1) {
-        for (i = 0; i < lengthMas; i++) {
-            while (j < 9) {
-                digits.number[i] = digits.number[i] + (buff[step - 1]-'0') * pow(10, j);
-                j++;
-                step--;
-                if (step == 0) return digits;
-            }
-            j = 0;
-        }
+    if (length == 1 && buff[0] == '0') {
+        digits.number[0] = 0;
     }
+
     else {
-        j = length;
-        step = 0;
-        while (j != 0) {
-            digits.number[lengthMas - 1] = digits.number[lengthMas - 1] + (buff[step] - '0') * pow(10, j - 1);
-            j--;
-            step++;
+
+        int step = length;
+        int j = 0;
+
+        if (lengthMas > 1) {
+            for (i = 0; i < lengthMas; i++) {
+                while (j < 9) {
+                    digits.number[i] = digits.number[i] + (buff[step - 1] - '0') * pow(10, j);
+                    j++;
+                    step--;
+                    if (step == 0) return digits;
+                }
+                j = 0;
+            }
+        } else {
+            j = length;
+            step = 0;
+            while (j != 0) {
+                digits.number[lengthMas - 1] = digits.number[lengthMas - 1] + (buff[step] - '0') * pow(10, j - 1);
+                j--;
+                step++;
+            }
         }
     }
     return digits;
@@ -55,18 +64,17 @@ uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs) {
     int digitsEnd_lhs;
     int digitsEnd_rhs;
     uint2022_t rezult;
-
     for (i = 1; i < 68; i++) {
         if (lhs.number[i] == 0 && lhs.number[i - 1] != 0) {
             digitsEnd_lhs = i - 1; // i
             rezult = lhs;
         }
-
         if (rhs.number[i] == 0 && rhs.number[i - 1] != 0) {
             digitsEnd_rhs = i - 1; // i
             rezult = rhs;
         }
     }
+
 
     int length;
     int moreThanNine = 0;
@@ -74,26 +82,70 @@ uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs) {
         length = digitsEnd_lhs; // +1
     else
         length = digitsEnd_rhs; // +1
-
     for (i = length; i >= 0; i--) {
-            if (((lhs.number[i] / 100000000) + (rhs.number[i] / 100000000) % 10 + moreThanNine) > 9) {
-                moreThanNine = 1;
-                rezult.number[i] = 1000000000 - (lhs.number[i] + rhs.number[i]);
-            }
-        rezult.number[i] = lhs.number[i] + rhs.number[i];
+        rezult.number[i] += moreThanNine;
+        if ((lhs.number[i] + rhs.number[i] + moreThanNine) >= 1000000000) {
+            rezult.number[i] = (lhs.number[i] + rhs.number[i]) - 1000000000;
+            moreThanNine = 1;
         }
-
-
+        else {
+            rezult.number[i] = lhs.number[i] + rhs.number[i];
+            moreThanNine = 0;
+        }
+        rezult.number[i + 1] += moreThanNine;
+    }
     return rezult;
 }
 
 
 uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs) {
-    return uint2022_t();
+
+    uint2022_t rezult;
+
+    rezult = lhs;
+    int i;
+    int digitsEnd_lhs;
+    int digitsEnd_rhs;
+    bool flag = true;
+
+    for (i = 0; i < 68; i++) {
+        if (lhs.number[i] != rhs.number[i]) {
+            flag = false;
+        }
+    }
+
+    for (i = 1; i < 68; i++) {
+        if (rhs.number[i] == 0 && rhs.number[i - 1] != 0) {
+            digitsEnd_rhs = i - 1;
+//            rezult = rhs;
+        }
+    }
+
+    rezult = lhs;
+
+    int length = digitsEnd_rhs;
+
+    int lessThenZero = 0;
+    for (i = length; i >= 0; i--) {
+        if (lhs.number[i] < rhs.number[i]) {
+//            lessThenZero = 1;
+            rezult.number[i + 1] -= 1;
+            rezult.number[i] = (lhs.number[i] - rhs.number[i]) + 1000000000;
+        }
+        else rezult.number[i] = lhs.number[i] - rhs.number[i];
+    }
+    return rezult;
 }
 
+
 uint2022_t operator*(const uint2022_t& lhs, const uint2022_t& rhs) {
-    return uint2022_t();
+    uint2022_t rezult;
+
+    int i;
+    int digitsEnd_lhs;
+    int digitsEnd_rhs;
+
+    return rezult;
 }
 
 uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs) {
@@ -102,6 +154,7 @@ uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs) {
 
 bool operator==(const uint2022_t& lhs, const uint2022_t& rhs) {
     bool flag = true;
+
     for (int i = 0; i < 68; i++) {
         if (lhs.number[i] != rhs.number[i]) {
             flag = false;
@@ -114,9 +167,18 @@ bool operator==(const uint2022_t& lhs, const uint2022_t& rhs) {
 
 bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs) {
     bool flag = true;
+    int digitsEnd = 0;
+
     for (int i = 0; i < 68; i++) {
+        if (lhs.number[i] == 0 && lhs.number[i - 1] != 0) {
+            digitsEnd = i - 1;
+        }
+    }
+
+    for (int i = 0; i < digitsEnd; i++) {
         if (lhs.number[i] == rhs.number[i]) {
             flag = false;
+            break;
         }
     }
 
@@ -142,35 +204,33 @@ std::ostream& operator<<(std::ostream& stream, const uint2022_t& value) {
     for (i = digitsEnd; i > 0; i--) {
         countDigits = 0;
         subNumber=value.number[i - 1];
-        if (value.number[i-1] == 0) {
+        if (value.number[i - 1] == 0) {
             stream << "000000000";
         }
 
-        if (i == digitsEnd) {
+        else if (i == digitsEnd) {
             stream << value.number[i-1];
         }
 
-        else if (value.number[i - 1] < 100000000 && i != digitsEnd){
+        else if (value.number[i - 1] < 100000000 && i != digitsEnd) {
             while(subNumber != 0){
                 countDigits++;
                 subNumber= subNumber / 10;
             }
             int countLeftZero = 9 - countDigits;
+
             for(j = 0; j < countLeftZero; j++){
                 stream << "0";
             }
             stream << value.number[i-1];
         }
+
         else {
             stream << value.number[i-1];
         }
     }
 
-    std::cout << std::endl;
-
-//    for (i = 0; i < 67; i++) {
-//        std::cout << value.number[i] << " number - " << i << std::endl;
-//    }
+    if (digitsEnd == 0) stream << "0";
 
     return stream;
 }
